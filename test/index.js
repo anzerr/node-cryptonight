4fae039f465df6740e26f62bbb0eb43dd3eefa84
anzerr <ryan.anzer@epitech.eu>
2019-07-30 17:31:00 +0200

    🚧 update tests

--- test/index.js
+++ test/index.js
@@ -1,37 +1,55 @@
 
-require("./tests/compare_both.js");
-require("./tests/test.js");
-require("./tests/test_async.js");
-require("./tests/test_async-wow.js");
-require("./tests/test_async_light.js");
-require("./tests/test_async_heavy.js");
-require("./tests/test_async_pico.js");
-require("./tests/test_sync-1.js");
-require("./tests/test_sync-2.js");
-require("./tests/test_sync-r.js");
-require("./tests/test_sync-half.js");
-require("./tests/test_sync-wow.js");
-require("./tests/test_sync-xtl.js");
-require("./tests/test_sync-msr.js");
-require("./tests/test_sync-xao.js");
-require("./tests/test_sync-rto.js");
-require("./tests/test_sync-gpu.js");
-require("./tests/test_sync-rwz.js");
-require("./tests/test_sync-zls.js");
-require("./tests/test_sync-double.js");
-require("./tests/test_sync.js");
-require("./tests/test_sync_light.js");
-require("./tests/test_sync_light-1.js");
-require("./tests/test_sync_heavy.js");
-require("./tests/test_sync_heavy-xhv.js");
-require("./tests/test_sync_heavy-tube.js");
-require("./tests/test_sync_pico.js");
-require("./tests/test_perf.js");
-require("./tests/test_perf_light.js");
-require("./tests/test_perf_heavy.js");
-require("./tests/test_perf_gpu.js");
-require("./tests/test_perf_wow.js");
-require("./tests/test_perf_pico.js");
-require("./tests/test_perf_rwz.js");
-require("./tests/test_perf_zls.js");
-require("./tests/test_perf_double.js");
+const cp = require('child_process');
+
+const base = require('path').resolve(__dirname).replace(/\\/g, '/');
+
+const run = (test) => {
+	return new Promise((resolve) => cp.fork(`${base}/${test}`).on('error', console.log).on('exit', resolve));
+};
+
+let test = [
+	'tests/compare_both.js',
+	'tests/test.js',
+	'tests/test_async.js',
+	'tests/test_async-wow.js',
+	'tests/test_async_light.js',
+	'tests/test_async_heavy.js',
+	'tests/test_async_pico.js',
+	'tests/test_sync-1.js',
+	'tests/test_sync-2.js',
+	'tests/test_sync-r.js',
+	'tests/test_sync-half.js',
+	'tests/test_sync-wow.js',
+	'tests/test_sync-xtl.js',
+	'tests/test_sync-msr.js',
+	'tests/test_sync-xao.js',
+	'tests/test_sync-rto.js',
+	'tests/test_sync-gpu.js',
+	'tests/test_sync-rwz.js',
+	'tests/test_sync-zls.js',
+	'tests/test_sync-double.js',
+	'tests/test_sync.js',
+	'tests/test_sync_light.js',
+	'tests/test_sync_light-1.js',
+	'tests/test_sync_heavy.js',
+	'tests/test_sync_heavy-xhv.js',
+	'tests/test_sync_heavy-tube.js',
+	'tests/test_sync_pico.js',
+	'tests/test_perf.js',
+	'tests/test_perf_light.js',
+	'tests/test_perf_heavy.js',
+	'tests/test_perf_gpu.js',
+	'tests/test_perf_wow.js',
+	'tests/test_perf_pico.js',
+	'tests/test_perf_rwz.js',
+	'tests/test_perf_zls.js',
+	'tests/test_perf_double.js'
+];
+
+let p = Promise.resolve();
+for (let i in test) {
+	((t) => {
+		p = p.then(() => run(t));
+	})(test[i]);
+}
+p.then(() => console.log('done'));
